c5f722624228b4d613c77bea99ceac0bba53d124
jimhu <>
2007-03-16 02:36:05 +0000

    init.inc.php from phpicalendar 2.23rc1

---
+++ phpicalendar3/functions/init.inc.php
@@ -0,0 +1,20 @@
+<?php
+/*=====================init.inc.php=====================
+For phpicalendar 3
+
+Need to set:
+
+1. configurations
+	paths
+	languages
+	templates
+	behaviors
+2. calendar list
+3. range of dates to process
+
+*/
+
+
+
+
+?>
