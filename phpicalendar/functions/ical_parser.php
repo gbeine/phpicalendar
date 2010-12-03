e34f5f7afe944f8f5dc00b140a89cbee9e7d5966
parasytic <>
2010-12-03 20:20:20 +0000

    Remote web calendars should be copied to the temp directory from configuration, not the system temp.

--- phpicalendar/functions/ical_parser.php
+++ phpicalendar/functions/ical_parser.php
@@ -104,7 +104,7 @@ foreach ($cal_filelist as $cal_key=>$filename) {
 			 * - And again to collect the remaining data
 			 * See: http://phpicalendar.net/forums/viewtopic.php?f=45&t=4140#p14451
 			 */
-			$filename = tempnam(sys_get_temp_dir(), 'ICS');
+			$filename = tempnam($phpiCal_config->tmp_dir, 'ICS');
 			if (copy($cal_httpPrefix, $filename) === FALSE) {
 				exit(error($lang['l_copy_error'], $cal_httpPrefix));
 			}
